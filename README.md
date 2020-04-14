646c407d9efad461d1461c55e8f2731cd6fb1d62
gioperalto <gyoperalto@gmail.com>
2020-04-13 22:20:07 -0400

    Added in command-line argument options to pass in bells (changes image in the middle); added code documentation

--- README.md
+++ README.md
@@ -4,9 +4,14 @@ This program runs a turtle race with all of the players you enter into the playe
 ![Alt](/images/turnip-turtles.png "Turnip Turtles")
 
 ## Instructions (how to run the program) from a Terminal/Python client:
-1. Make sure all dependencies are installed (I use Python 3.x):
-    - `pip install numpy`
-2. To run the program: `python battlefield.py`
+1. Have Python (and pip) installed (I use Python 3.x)
+2. Install the dependencies:
+    - `pip install numpy`               (for Math stuff)
+    - `pip install sys`                 (for command line arguments)
+3. To run the program: 
+    - `python battlefield.py`           (creates program with default image)
+    - `python battlefield.py turnip`    (creates program with turnip image)
+    - `python battlefield.py bells`     (will create program with default image)
 
 ## To change the players:
 - There is a `players.txt` file where you can change out the names
--- battlefield.py
+++ battlefield.py
@@ -1,16 +1,27 @@
 from turnip_turtle import TurnipTurtle
-import turtle
+import turtle, sys
 
 def collision(a, b):
+  """
+  Determines if object a and b are intersecting.
+  @param a first object
+  @param b second object
+  @return True or False
+  """
   return abs(a.xcor() - b.xcor()) < 25 and abs(a.ycor() - b.ycor()) < 25
 
-if __name__ == "__main__":
-  space, turnip = turtle.Screen(), turtle.Turtle()
-  space.register_shape('images/turnip.gif'), turnip.shape('images/turnip.gif')
+def race(arg):
+  """
+  Simulates a turtle race between all of the players in players.txt. 
+  The objective of this race is to reach the target first.
+  @param target the target image to race to (a stationary turtle)
+  """
+  space, target = turtle.Screen(), turtle.Turtle()
+  space.register_shape('images/{}.gif'.format(arg)), target.shape('images/{}.gif'.format(arg))
   turtles, players = [], open('players.txt').read().split()
 
   for player in players:
-      turtles.append(TurnipTurtle(turtle=turtle, turnip=turnip, name=player, screensize=space.screensize()))
+      turtles.append(TurnipTurtle(turtle=turtle, target=target, name=player, screensize=space.screensize()))
 
   for turt in turtles:
       turt.info()
@@ -21,9 +32,17 @@ if __name__ == "__main__":
       turt.random_turn()
       turt.move()
 
-      if collision(turt.turtle, turnip):
+      if collision(turt.turtle, target):
         done = True
         turt.win()
         break
 
   space.exitonclick()
+
+if __name__ == "__main__":
+  if len(sys.argv) != 2:
+    experiment = 'turnip'
+  else:
+    experiment = sys.argv[1]
+
+  race(experiment)
